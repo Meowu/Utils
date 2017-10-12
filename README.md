e92852a05db250c0f787c03c1b710ef330848fcb
Meowu <474384902@qq.com>
2017-10-12 19:19:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A collection of reusable util functions written in vanilla JavaScript.
---
+++ pick.js
@@ -0,0 +1,24 @@
+
+/**
+ * 从传入对象中选择指定属性组成一个新对象
+ * @param {Object} object 
+ * @param {String} props 
+ * @return {Object}
+ * example pick({a: 1, b: 2, c: 3}, 'a', 'c')
+ * => {a: 1, c: 3}
+ */
+function pick(object, ...props) {
+  if (Object.prototype.toString.call(object) !== "[object Object]") {
+    throw new Error("It must be given an object.")
+  }
+  return Object.assign(
+    {},
+    ...props.map(prop => {
+      if (prop in object) {
+        return {
+          [prop]: object[prop]
+        }
+      }
+    })
+  );
+}
