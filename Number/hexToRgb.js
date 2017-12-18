ee0eeaf690bdce46586ef4a33b96fbecfe1d91bd
Meowu <474384902@qq.com>
2017-12-18 23:41:39 +0800

    add rgbToHex.js, make hexToRgb more accurate and be able to accept 3 num

--- Number/hexToRgb.js
+++ Number/hexToRgb.js
@@ -1,7 +1,18 @@
 export default (hex) => {
   hex = hex.slice(1)
-  const str1 = hex.slice(0, 2)
-  const str2 = hex.slice(2, 4)
-  const str3 = hex.slice(4)
+  let str1
+  let str2
+  let str3
+  if (hex.length === 3) {
+    str1 = String(hex[0]) + hex[0]
+    str2 = String(hex[1]) + hex[1]
+    str3 = String(hex[2]) + hex[2]
+  } else if (hex.length === 6) {
+    str1 = hex.slice(0, 2)
+    str2 = hex.slice(2, 4)
+    str3 = hex.slice(4)
+  } else {
+    throw new Error('Hex str must be 3 or 6 at length, got: ' + hex)
+  }
   return `rgb(${parseInt(str1, 16)}, ${parseInt(str2, 16)}, ${parseInt(str3, 16)})`
 }
--- Number/rgbToHex.js
+++ Number/rgbToHex.js
@@ -1,3 +1,16 @@
 export default (rgb) => {
-  
+  const matchStr = rgb.match(/(\d{1,3})/g) // 带有g标志，要返回所有的匹配结果，使用match，如果要返回匹配组则使用exec。
+  const toNum = matchStr.map(str => {
+    const num = Number(str)
+    if (0 <= num <= 255) {
+     return num 
+    }
+    throw new Error('RGB value must be between 0 ~ 255: ' + num)
+  })
+  const hexArr = Array.from(toNum, num => {
+    num = num.toString(16)
+    num < 10 && (num = '0' + num) // 如果数字小于10则补0
+    return num
+  })
+  return '#' + hexArr.join('')
 }
