494a86775773b3944945ea1b7217009d3777ae89
Meowu <474384902@qq.com>
2017-12-22 23:43:50 +0800

    assure correct type of params while generating rgba color

--- Number/generateRgbaColor.js
+++ Number/generateRgbaColor.js
@@ -2,6 +2,10 @@
  * 随机生成rgb颜色，可选透明度。
  */
 export default (alpha=1) => {
+  const isValidAlpha = typeof alpha === 'number' && alpha >= 0 && alpha <= 1
+  if (!isValidAlpha) {
+    throw new Error('Invalid alpha: must be between 0 ~ 1.')
+  }
   const random = () => Math.round(Math.random()*255)
   return `rgba(${random()}, ${random()}, ${random()}, ${alpha})`
 }
